c6abbd3eeaa483415b499c1cd8d4234e7c6ed806
prashantpiyush <piyush.5862@gmail.com>
2017-01-14 10:52:29 +0530

    Merge Sort

--- Algorithms/Merge sort.java
+++ Algorithms/Merge sort.java
@@ -1,4 +1,5 @@
 /**
+* <h1>Merge Sort</h1>
 * This is a sorting program which uses mergesort algorithm
 * to sort an array of integers.
 *
@@ -8,17 +9,17 @@ import java.util.Arrays;
 import java.io.*;
 
 class Mergesort {
-        
+    
+    /**
+    * This method divides unsorted array into subarrays
+    * which are eventually sorted and the merged to form
+    * a sorted array.
+    *
+    * @param arr  Array to be sorted
+    * @param low  Lowest index of the subarray formed
+    * @param high Highest index of the subarray formed
+    */
     static void sort(int[] arr, int low, int high) {
-        /**
-        * This method divides unsorted array into subarrays
-        * which are eventually sorted and the merged to form
-        * a sorted array.
-        *
-        * @param arr Array to be sorted
-        * @param low Lowest index of the subarray formed
-        * @param high Highest index of the subarray formed
-        */
         if(low < high) {
             int mid = low + (high - low)/2;
             sort(arr, low, mid);
@@ -27,17 +28,17 @@ class Mergesort {
         }
     }
     
+    /**
+    * Merges subarrays while sorting them until only the
+    * final sorted array remains.
+    *
+    * @param arr
+    * @param low  Min index of first subarray
+    * @param mid  Max index of first subarray, also (mid+1)
+    *             is the min indes of second subarray
+    * @param high Max index of second subarray
+    */
     static void merge(int[] arr, int low, int mid, int high) {
-        /**
-        * Merges subarrays while sorting them until only the
-        * final sorted array remains.
-        *
-        * @param arr
-        * @param low Min index of first subarray
-        * @param mid Max index of first subarray, also (mid+1)
-        *           is the min indes of second subarray
-        * @param high Max index of second subarray
-        */
         int temp[] = new int[arr.length];
         for(int p=0; p<arr.length; p++){
             temp[p] = arr[p];
@@ -72,13 +73,13 @@ class Mergesort {
         }
     }
     
+    /**
+    * This is main method. It inputs unsorted array and calls
+    * sort() to start merge sort.
+    * 
+    * @throws IOException
+    */
     public static void main(String args[]) throws IOException {
-        /**
-        * This is main method. It inputs unsorted array and calls
-        * sort() to start merge sort.
-        * 
-        * @throws IOException
-        */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         // Take input as string array
